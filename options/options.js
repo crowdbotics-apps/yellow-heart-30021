4570aacea6443b8852ea80cc6b242345767b4727
Crowdbotics <team@crowdbotics.com>
2021-08-22 11:32:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "yellow_heart_30021Identifier",
+  url: "https://yellow_heart_30021Identifier.botics.co",
+  api: "https://yellow_heart_30021Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
